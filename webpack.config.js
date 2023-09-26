fad12c478a6a22a56c7b4005da27f745d22678ad
Dima Danilov <dima.danilov.2012@mail.ru>
2023-09-26 20:11:50 +0300

    init webpack commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: ["file-loader"],
+      },
+      //   {
+      //     test: /\.js$/,
+      //     exclude: /node_modules/,
+      //     loader: {
+      //       loader: "babel-loader",
+      //       options: {
+      //         presets: ["@babel/preset-env"],
+      //       },
+      //     },
+      //   },
+      //   {
+      //     test: /\.jsx$/,
+      //     exclude: /node_modules/,
+      //     loader: {
+      //       loader: "babel-loader",
+      //       options: {
+      //         presets: ["@babel/preset-react", "@babel/preset-env"],
+      //       },
+      //     },
+      //   },
+    ],
+  },
+};
