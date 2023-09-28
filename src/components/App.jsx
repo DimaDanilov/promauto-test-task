d7ade11ac3ba4023a94fb07b56c2b24a015eee89
Dima Danilov <dima.danilov.2012@mail.ru>
2023-09-28 20:03:56 +0300

    add: jsx config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { UserList } from "./UserList/UserList";
 
 export const App = () => {
   return (
     <div>
-      <h1>TEST COMPONENT</h1>
+      <UserList />
     </div>
   );
 };
