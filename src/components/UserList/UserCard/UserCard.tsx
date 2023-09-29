8d0f66e5fe93e732bb90bde829bf113949ca182f
Dima Danilov <dima.danilov.2012@mail.ru>
2023-09-29 10:48:43 +0300

    cards base template

--- src/components/UserList/UserCard/UserCard.tsx
+++ src/components/UserList/UserCard/UserCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { UserModel } from "../../../models/UserModel";
+import "./UserCard.less";
 
 export const UserCard = ({ user }: { user: UserModel }) => {
   return (
-    <div>
+    <div className="user-card">
       <h1>{user.email}</h1>
     </div>
   );
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { getUsers } from "../../api/UserApi";
 import { UserModel } from "../../models/UserModel";
 import { UserCard } from "./UserCard/UserCard";
+import "./UserList.less";
 
 export const UserList = () => {
   const [userList, setUserList] = useState<UserModel[]>([]);
@@ -15,5 +16,5 @@ export const UserList = () => {
 
   const userCards = userList.map((u) => <UserCard user={u} />);
 
-  return <div>{userCards}</div>;
+  return <div className="user-cards-container">{userCards}</div>;
 };
